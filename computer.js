e069628fbae97018a987046e81465c57f24e1bc8
Anirudh Upadhya K <64802718+AnirudhUK1999@users.noreply.github.com>
2023-12-21 19:17:10 +0530

    Added Ui and scorecard to RPS

--- computer.js
+++ computer.js
@@ -1,3 +1,5 @@
+
+
 const choices = ['rock','paper','scissor']
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)]
@@ -12,62 +14,72 @@ function playRound(computerSelection,playerSelection){
     let compTemplate = "Comp Won";
     if (computerSelection == choices[0] && playerSelection == choices[1]){
         res = playerTemplate + ", Paper beats Rock"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else if(computerSelection == choices[1] && playerSelection == choices[0]){
         res = compTemplate + ", Paper beats Rock"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else if(computerSelection == choices[1] && playerSelection == choices[2]){
         res = playerTemplate +", Scissors beats Paper"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else if(computerSelection == choices[2] && playerSelection == choices[1]){
         res = compTemplate + ", Scissors beats Paper"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else if(computerSelection == choices[2] && playerSelection == choices[0]){
         res = playerTemplate +", Rock beats paper"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else if(computerSelection == choices[0] && playerSelection == choices[2]){
         res = compTemplate + ", Rock beats paper"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     else{
         res = "Draw! Play Again?"
+        // console.log(playerTemplate + ", Paper beats Rock")
     }
     return res
 }
-
-function game(){
-    let scorePlayer = 0;
-    let scoreComp = 0;
-    //best of five
-    for(let i = 0;i<5;){
-        //user input
-        let playerSelection = prompt("Choose !",choices[0])
-        
-        computerSelection = getComputerChoice().toString().toLowerCase()
-        playerSelection = playerSelection.toLowerCase()
-        
-        let winner = playRound(computerSelection,playerSelection)
-        
-        console.log(winner);   // log round winner
-        //determine score card     
-        if(winner.includes("Player Won")){
-            scorePlayer++;
-        }
-        else if((winner.includes("Comp Won"))) {
-            scoreComp ++;
-        }
-        else    // if draw, replay again
-            continue;
-        i++;
-        if(scorePlayer == 3 || scoreComp == 3 )
-            break;
+let scorePlayer = 0;
+let scoreComp = 0;
+let result = ""
+let finalDisplay = undefined;
+function game(playerSelection){
+    computerSelection = getComputerChoice().toString().toLowerCase()
+    let winner = playRound(computerSelection,playerSelection);
+    let buttonContainer = document.querySelector('.buttonContainer');
+    if(scorePlayer == 5){
+        result = "Player has won!";
+        finalDisplay = document.createElement('div');
+        finalDisplay.classList.add('finalDisplay-container');
+        finalDisplay.textContent = result;
+        buttonContainer.appendChild(finalDisplay);
+        return;
     }
-
-    //final display
-    if(scorePlayer > scoreComp)
-        console.log("Player has beat Comp!")
-    else if (scorePlayer == scoreComp)
-        console.log("Its a draw ! ");
-    else
-        console.log("Comp has beat you, Sorry !! :(");
-    
+    else if(scoreComp == 5){
+        result = "Comp has won!";
+        finalDisplay = document.createElement('div');
+        finalDisplay.classList.add('finalDisplay-container');
+        finalDisplay.textContent = result;
+        buttonContainer.appendChild(finalDisplay);
+        return;
+    }
+    if(winner.includes("Player Won")){
+        scorePlayer++;
+    }
+    else if((winner.includes("Comp Won"))) {
+        scoreComp ++;
+    }
+    let display = document.createElement('div');
+    display.classList.add('display-container');
+    let scoreCard = "PlayerScore: "+ scorePlayer + " ; " + "CompScore: " + scoreComp;
+    display.textContent = scoreCard;
+    buttonContainer.appendChild(display)
+    console.log("scorePlayer: "+scorePlayer);
+ 
 }
+
+
+
