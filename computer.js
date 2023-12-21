0697042c7aa2c0d494f4c1b5302e93a33dd83cb6
Anirudh Upadhya K <64802718+AnirudhUK1999@users.noreply.github.com>
2023-12-21 16:49:21 +0530

    Added game function and input prompts along with Best of five

--- computer.js
+++ computer.js
@@ -1,41 +1,73 @@
-const choices = ['rock','paper','scissors']
+const choices = ['rock','paper','scissor']
 function getComputerChoice(){
-    // console.log("Computer Choice: "+choices[Math.floor(Math.random() * 3)])
     return choices[Math.floor(Math.random() * 3)]
 }
 
-// function getComputerChoice(){
-//     console.log("Computer Choice: "+choices[Math.floor(Math.random() * 3)])
-//     return choices[Math.floor(Math.random() * 3)]
-// }
 
-function result(choice){
+function playRound(computerSelection,playerSelection){
     let res = ""
-    let choiceComp = getComputerChoice().toString().toLowerCase()
-    let choicePlayer = choice.toString().toLowerCase()
-    // console.log(getComputerChoice().toString().toLowerCase())
-    console.log(choiceComp)
-    console.log(choicePlayer)
-    if (choiceComp == choices[0] && choicePlayer == choices[1]){
-        res = "Player Won, Paper beats Rock"
+    console.log("comp choice: "+computerSelection)
+    console.log("player choice: "+playerSelection)
+    let playerTemplate = "Player Won";
+    let compTemplate = "Comp Won";
+    if (computerSelection == choices[0] && playerSelection == choices[1]){
+        res = playerTemplate + ", Paper beats Rock"
     }
-    else if(choiceComp == choices[1] && choicePlayer == choices[0]){
-        res = "Comp Won, Paper beats Rock"
+    else if(computerSelection == choices[1] && playerSelection == choices[0]){
+        res = compTemplate + ", Paper beats Rock"
     }
-    else if(choiceComp == choices[1] && choicePlayer == choices[2]){
-        res = "Player Won, Scissors beats Paper"
+    else if(computerSelection == choices[1] && playerSelection == choices[2]){
+        res = playerTemplate +", Scissors beats Paper"
     }
-    else if(choiceComp == choices[2] && choicePlayer == choices[1]){
-        res = "Comp Won, Scissors beats Paper"
+    else if(computerSelection == choices[2] && playerSelection == choices[1]){
+        res = compTemplate + ", Scissors beats Paper"
     }
-    else if(choiceComp == choices[2] && choicePlayer == choices[0]){
-        res = "Player Won, Rock beats paper"
+    else if(computerSelection == choices[2] && playerSelection == choices[0]){
+        res = playerTemplate +", Rock beats paper"
     }
-    else if(choiceComp == choices[0] && choicePlayer == choices[2]){
-        res = "Comp Won, Rock beats paper"
+    else if(computerSelection == choices[0] && playerSelection == choices[2]){
+        res = compTemplate + ", Rock beats paper"
     }
     else{
         res = "Draw! Play Again?"
     }
     return res
+}
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComp = 0;
+    //best of five
+    for(let i = 0;i<5;){
+        //user input
+        let playerSelection = prompt("Choose !",choices[0])
+        
+        computerSelection = getComputerChoice().toString().toLowerCase()
+        playerSelection = playerSelection.toLowerCase()
+        
+        let winner = playRound(computerSelection,playerSelection)
+        
+        console.log(winner);   // log round winner
+        //determine score card     
+        if(winner.includes("Player Won")){
+            scorePlayer++;
+        }
+        else if((winner.includes("Comp Won"))) {
+            scoreComp ++;
+        }
+        else    // if draw, replay again
+            continue;
+        i++;
+        if(scorePlayer == 3 || scoreComp == 3 )
+            break;
+    }
+
+    //final display
+    if(scorePlayer > scoreComp)
+        console.log("Player has beat Comp!")
+    else if (scorePlayer == scoreComp)
+        console.log("Its a draw ! ");
+    else
+        console.log("Comp has beat you, Sorry !! :(");
+    
 }
